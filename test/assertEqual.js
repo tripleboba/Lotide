c55567d8de4b1337ba2e625ff4ce9ddb63f77a69
Triple the Boba ! <coconuttt1010@gmail.com>
2022-01-10 22:47:29 -0800

    ...

--- test/assertEqual.js
+++ test/assertEqual.js
@@ -1,4 +1,4 @@
-// Comparing 2 primitive variables if it's the same
+// Comparing 2 primitive variables whether it's the same
 const assertEqual = (actual, expect, msg = "note?") => {
   console.log(`Actual Output: ${trueType(actual)} || Expect Outputs: ${trueType(expect)}`);
   actual === expect ? console.log(`✅✅ Pass: ${trueType(actual)} === ${trueType(expect)}\n`)
