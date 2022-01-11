46f9af5e284e28aba7fbdbd5c3fb762a501e1d93
Triple the Boba ! <coconuttt1010@gmail.com>
2022-01-10 23:17:51 -0800

    refactor shorter code

--- tail.js
+++ tail.js
@@ -1,13 +1,16 @@
 // EVERY ELEMENTS AFTER THE HEAD - not just the last one
 const tail = arr => {
-  if (!arr.length) return undefined;
-  else {
-    const result = [];
-    for (let i = 1; i < arr.length; i++) {
-      result.push(arr[i]);
-    }
-    return result;
-  }
+  // if (!arr.length) return undefined;
+  // else {
+  //   const result = [];
+  //   for (let i = 1; i < arr.length; i++) {
+  //     result.push(arr[i]);
+  //   }
+  //   return result;
+  // }
+
+  // refactor shorter code
+  return arr.length ? arr.slice(1) : undefined;
 };
 
 module.exports = tail;
