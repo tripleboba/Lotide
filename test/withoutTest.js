3a8740debf04454d1746ccc6176735bd600de5b6
Triple the Boba ! <coconuttt1010@gmail.com>
2022-01-12 00:57:53 -0800

    add chai withoutTest

--- test/withoutTest.js
+++ test/withoutTest.js
@@ -1,31 +1,33 @@
-// TEST CASE using assertArraysEqual
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) return false;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-  return true;
-};
-const trueType =  (value) => {
-  const arrString = value.map(x => typeof x === 'string' ? "\"" + x + "\"" : x);
-  const result = "[" + arrString.join(", ") + "]";
-  return result;
-};
-const assertArraysEqual = (actual, expect) => {
-  const value1 = trueType(actual);
-  const value2 = trueType(expect);
-  console.log(`--Comparing ${value1} vs ${value2}`);
-  eqArrays(actual, expect) ? console.log(`✅ Pass: ${value1} === ${value2}`)
-                           : console.log(`❌ Fail: ${value1} !== ${value2}`);
-};
+const without = require('../without');
+// const assertArraysEqual = require('./assertArraysEqual');
 
+// // TEST CASE using assertArraysEqual
+// console.log('----- Test Case 1 ----');
 const test1 = without([1, 2, 3], [1]); // => [2, 3]
-assertArraysEqual(test1, [2, 3]);
+// assertArraysEqual(test1, [2, 3]);
 
+// console.log('----- Test Case 2 ----');
 const test2 = without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
-assertArraysEqual(test2, ["1", "2"]);
+// assertArraysEqual(test2, ["1", "2"]);
 
-// test for the without() is return new [], not modify the original []
-const test3 = ["hello", "world", "lighthouse"];
-without(test3, ["lighthouse"]);
-assertArraysEqual(test3, ["hello", "world", "lighthouse"]);
+// console.log('----- Test Case 3: not modify origin array ----');
+// // test for the without() is return new [], not modify the original []
+// const test3 = ["hello", "world", "lighthouse"];
+// without(test3, ["lighthouse"]);
+// assertArraysEqual(test3, ["hello", "world", "lighthouse"]);
+
+// test using chai & mocha
+const assert = require('chai').assert.deepEqual;
+describe("#without", () => {
+  it("Return [2, 3] for without([1, 2, 3])", () => {
+    assert(test1, [2, 3]);
+  });
+  it("Remove and return existing values in the array only", () => {
+    assert(test2, ["1", "2"]);
+  });
+  it("without() doesn't modify original array", ()  => {
+    const test3 = ["hello", "world", "lighthouse"];
+    without(test3, ["lighthouse"]);
+    assert(test3,["hello", "world", "lighthouse"]);
+  });
+});
