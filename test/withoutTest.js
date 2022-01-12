a5c0776f43ec37e05f32016d1404a0b58f8c91ec
Triple the Boba ! <coconuttt1010@gmail.com>
2022-01-12 00:59:44 -0800

    add chai withoutTest

--- test/withoutTest.js
+++ test/withoutTest.js
@@ -3,11 +3,11 @@ const without = require('../without');
 
 // // TEST CASE using assertArraysEqual
 // console.log('----- Test Case 1 ----');
-const test1 = without([1, 2, 3], [1]); // => [2, 3]
+// const test1 = without([1, 2, 3], [1]); // => [2, 3]
 // assertArraysEqual(test1, [2, 3]);
 
 // console.log('----- Test Case 2 ----');
-const test2 = without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
+// const test2 = without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
 // assertArraysEqual(test2, ["1", "2"]);
 
 // console.log('----- Test Case 3: not modify origin array ----');
@@ -20,9 +20,11 @@ const test2 = without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
 const assert = require('chai').assert.deepEqual;
 describe("#without", () => {
   it("Return [2, 3] for without([1, 2, 3])", () => {
+    const test1 = without([1, 2, 3], [1]);
     assert(test1, [2, 3]);
   });
   it("Remove and return existing values in the array only", () => {
+    const test2 = without(["1", "2", "3"], [1, 2, "3"]);
     assert(test2, ["1", "2"]);
   });
   it("without() doesn't modify original array", ()  => {
